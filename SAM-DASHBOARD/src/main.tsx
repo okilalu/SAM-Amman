122970b85ab98d089bb02059455faafaf3ab9c8e
okilalu <laluoki66@gmail.com>
2025-10-12 13:23:35 +0800

    [FE] "Handle utils and main"

--- SAM-DASHBOARD/src/main.tsx
+++ SAM-DASHBOARD/src/main.tsx
@@ -1,12 +1,17 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import store from "./Redux/store.ts";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 );
--- SAM-DASHBOARD/utils/uri.ts
+++ SAM-DASHBOARD/utils/uri.ts
@@ -1,1 +1,2 @@
-export const uri = "https://dt974l71-2090.asse.devtunnels.ms/";
+export const uri = "http://localhost:2090";
+// export const uri = "https://dt974l71-2090.asse.devtunnels.ms";
