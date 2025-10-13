b6451c734638edac293e509b54abfa41faad91c8
okilalu <laluoki66@gmail.com>
2025-10-13 17:04:51 +0800

    [FE] "Add route for login and register"

--- SAM-DASHBOARD/src/App.tsx
+++ SAM-DASHBOARD/src/App.tsx
@@ -1,11 +1,29 @@
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Sidebar />
-      </div>
+    <div className="min-h-screen">
+      <Routes>
+        {/* Route untuk halaman login */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Route untuk halaman register */}
+        <Route path="/register" element={<Register />} />
+
+        {/* Route default (misalnya dashboard) */}
+        <Route
+          path="/*"
+          element={
+            <div>
+              <Sidebar />
+              {/* konten utama bisa kamu isi di sini */}
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 }
