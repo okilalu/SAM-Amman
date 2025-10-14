8b23d6143188e55cee0d67dee7b0019cd3f05333
laluferdianyusuf <lferdianyusuf@gmail.com>
2025-10-14 16:06:42 +0800

    [FE] "New styling for inputs - remove flex-1 at label"

--- SAM-DASHBOARD/src/components/CustomInputs.tsx
+++ SAM-DASHBOARD/src/components/CustomInputs.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PiCalendarDots } from "react-icons/pi";
 
 interface CustomInputsProps {
   label: string;
@@ -24,33 +25,44 @@ export const CustomInputs: React.FC<CustomInputsProps> = ({
   id,
 }) => {
   const inputId = id || `input-${label.replace(/\s+/g, "-").toLowerCase()}`;
+  const hasHelper = String(value)?.length > 0 && helper;
 
   return (
     <div className={`${className} flex items-center gap-5`}>
       <label
         htmlFor={inputId}
-        className="flex-1 text-sm font-semibold text-gray-700"
+        className="min-w-24 text-sm font-semibold text-gray-700"
       >
         {label}
       </label>
-      <div className="flex flex-1 items-center relative">
+
+      <div className="flex w-full items-center relative">
         <input
           value={value}
           id={inputId}
           type={type}
           onChange={(e) => onChange?.(e.target.value)}
           placeholder={placeholder}
-          className="flex-1 p-2 py-1.5 border rounded-md border-gray-300 focus:outline-none focus:ring-0 focus:ring-blue-500"
+          className={`flex-1 p-2 py-1.5 border rounded-md border-gray-300 focus:outline-none focus:ring-0 ${
+            hasHelper && type !== "date" ? "pr-12" : ""
+          }`}
         />
-        {String(value)?.length! > 0 ? (
+
+        {type === "date" && (
+          <div className="absolute right-3 top-2 h-5 w-5 text-gray-500 pointer-events-none">
+            <PiCalendarDots size={20} className="" />
+          </div>
+        )}
+
+        {type !== "date" && hasHelper && (
           <button
             type="button"
             onClick={helper}
-            className="cursor-pointer rounded-md absolute right-0 px-3 py-3 no-underline text-blue-600 text-sm"
+            className="cursor-pointer rounded-md absolute right-2 top-1/2 -translate-y-1/2 text-blue-600 text-sm"
           >
             {helperText}
           </button>
-        ) : null}
+        )}
       </div>
     </div>
   );
