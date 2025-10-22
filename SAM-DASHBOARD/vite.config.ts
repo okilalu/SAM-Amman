7e05ea8bd8bfb5bc48a7bb24aaf4bb487302dc30
okilalu <laluoki66@gmail.com>
2025-10-22 14:07:36 +0800

    [FE] adding define config server

--- SAM-DASHBOARD/vite.config.ts
+++ SAM-DASHBOARD/vite.config.ts
@@ -5,6 +5,16 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://dt974l71-2090.asse.devtunnels.ms",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
