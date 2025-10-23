a81ce1d5e8981f6d16f0e6e4977cb7cc5e8def44
okilalu <laluoki66@gmail.com>
2025-10-23 21:55:10 +0800

    [FE] keep

--- SAM-DASHBOARD/utils/uri.ts
+++ SAM-DASHBOARD/utils/uri.ts
@@ -1,3 +1,3 @@
-export const uri = "http://localhost:2090";
-// export const uri = "https://dt974l71-2090.asse.devtunnels.ms";
+// export const uri = "http://localhost:2090";
+export const uri = "https://dt974l71-2090.asse.devtunnels.ms";
 // export const uri = "http://192.168.1.10:2090";
