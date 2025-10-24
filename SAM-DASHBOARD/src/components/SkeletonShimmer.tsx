949b414aedaf6bc0ca4e9ad203cc899d9588eb16
laluferdianyusuf <lferdianyusuf@gmail.com>
2025-10-24 10:48:49 +0800

    [FE] Fix styling of skeleton loading

--- SAM-DASHBOARD/src/components/SkeletonShimmer.tsx
+++ SAM-DASHBOARD/src/components/SkeletonShimmer.tsx
@@ -11,10 +11,10 @@ export const SkeletonShimmer: React.FC<SkeletonShimmerProps> = ({
   return (
     <div
       className={clsx(
-        "relative overflow-hidden bg-gray-300/40 rounded-lg",
+        "relative overflow-hidden bg-gray-100 dark:bg-gray-200",
         "before:absolute before:inset-0 before:-translate-x-full",
         "before:animate-[shimmer_1.5s_infinite]",
-        "before:bg-gradient-to-r before:from-transparent before:via-white/40 before:to-transparent",
+        "before:bg-gradient-to-r before:from-transparent before:via-white/50 before:to-transparent",
         className
       )}
     />
